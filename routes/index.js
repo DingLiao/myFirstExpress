b1f74c620354a43b6cb63b4aa471c1778383ed26
ding <ding@ding-iMac.local>
2017-05-23 23:04:19 +0300

    improve static html pages

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,31 @@ router.get('/login', function(req, res, next) {
 	res.render('login', {title: 'Login'});
 });
 
+router.post('/login', function(req, res, next) {
+	var user = {
+		username: 'admin',
+		password: 'admin'
+	};
+
+	if(req.body.username === user.username && req.body.password===user.password) {
+		res.redirect('/home');
+	} else {
+		res.redirect('/login');
+	}
+});
+
+router.get('/logout', function(req, res, next) {
+	res.redirect('/');
+});
+
+router.get('/home', function(req,res, next) {
+	var user = {
+		username: 'admin',
+		password: 'admin'
+	};
+
+	res.render('home', {title: 'Home', user: user});
+});
+
 
 module.exports = router;
